7611182a3c40a6f1073df431f4a899bc156b85f6
0u812 <0u812@users.noreply.github.com>
2015-07-17 11:29:21 -0700

    more splits

--- js/fix-email.js
+++ js/fix-email.js
@@ -3,8 +3,8 @@
   'use strict';
 
   var fixEmail = function () {
-    document.getElementById('jkmail').href = 'mailto:' + ['medleyj','uw.edu'].join('@');
-    document.getElementById('jkmail').innerHTML = ['medleyj','uw.edu'].join('@');
+    document.getElementById('jkmail').href = 'mailto:' + ['medleyj',['uw', 'edu'].join('.')].join('@');
+    document.getElementById('jkmail').innerHTML = ['medleyj',['uw', 'edu'].join('.')].join('@');
   };
 
   fixEmail();
